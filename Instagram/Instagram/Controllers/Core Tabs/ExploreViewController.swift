2150d2ddaaba809e8a1bd7a5673d028e9fe18879
Gabriel <tandohtakyie@hotmail.com>
2021-06-30 11:17:11 +0200

    First commit - added necessary views

---
+++ Instagram/Instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by Gabriel Takyi on 30/06/2021.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Gabriel Takyi on 30/06/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
