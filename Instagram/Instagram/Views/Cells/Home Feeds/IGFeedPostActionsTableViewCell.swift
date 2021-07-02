f5e9c5f2efa75a36ca8ef47d4614a386aa026781
Gabriel <tandohtakyie@hotmail.com>
2021-07-02 14:16:54 +0200

    Added elements to the settings screen, configured storagebucket firebase

---
+++ Instagram/Instagram/Views/Cells/Home Feeds/IGFeedPostActionsTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IGFeedPostActionsTableViewCell.swift
+//  Instagram
+//
+//  Created by Gabriel Takyi on 02/07/2021.
+//
+
+import UIKit
+
+class IGFeedPostActionsTableViewCell: UITableViewCell {
+
+
+}
---
+++ Instagram/Instagram/Views/Cells/Home Feeds/IGFeedPostHeaderTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IGFeedPostHeaderTableViewCell.swift
+//  Instagram
+//
+//  Created by Gabriel Takyi on 02/07/2021.
+//
+
+import UIKit
+
+class IGFeedPostHeaderTableViewCell: UITableViewCell {
+
+}
